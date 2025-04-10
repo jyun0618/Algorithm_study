57da090d4582f206be43d06bd4a215dc329563b2
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-04-10 17:07:39 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -0,0 +1,11 @@
+N = int(input())
+size = list(map(int, input().split()))
+T, P = map(int, input().split())
+shirts = 0
+for i in range(len(size)):
+    if size[i] % T == 0:
+        shirts += size[i] // T
+    else:
+        shirts += (size[i] // T) + 1
+print(shirts)
+print(N // P, N % P)
