864ef9fb72a057a41bfd8cd077467ac0a57fcdd6
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-04-27 17:43:42 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 37144 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,11 @@
+L = int(input())
+seq = list(input())
+import string
+alphabet = list(string.ascii_lowercase)
+h = 0
+for l in range(L):
+    for i in range(26):
+        if seq[l] == alphabet[i]:
+            h += (i+1)*31**l
+print(h)
+    
