99031e84aad199218ac6676460a58695d8d13a94
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-05-25 15:27:37 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -3,8 +3,8 @@ time = list(map(int, input().split()))
 Y = 0
 M = 0
 for t in time:
-    Y += (int(t) // 30 + 1) * 10
-    M += (int(t) // 60 + 1) * 15
+    Y += (t // 30 + 1) * 10
+    M += (t // 60 + 1) * 15
 
 if Y < M:
     print('Y', Y)
