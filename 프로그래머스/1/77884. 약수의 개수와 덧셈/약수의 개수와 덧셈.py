4cb4497a3109cc416ed3ca14fabf2ee3fcbda8a9
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-01-29 17:16:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.42 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,8 @@
+def solution(left, right):
+    answer = 0
+    for n in range(left, right+1):
+        if int(n ** 0.5) ** 2 == n:
+            answer -= n
+        else:
+            answer += n
+    return answer
