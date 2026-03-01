ff03ec762934b16b5519fd735b993db14715b151
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-03-01 16:13:22 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,9 +1,6 @@
 SELECT
     animal_type,
-    CASE
-        WHEN name IS NULL THEN 'No name'
-        ELSE name
-    END AS name,
+    IFNULL(name, 'No name') AS name,
     sex_upon_intake
 FROM animal_ins
 ORDER BY animal_id
